ae1ba64c3fcfe0dd6763200111680ee95a4c1860
Ovanes <o-zakharyan@mail.ru>
2021-01-21 19:05:10 +0300

    task_2

--- lesson4.playground/Contents.swift
+++ lesson4.playground/Contents.swift
@@ -2,3 +2,42 @@ import UIKit
 
 
 
+
+enum Transmission {
+    case auto, manual
+}
+
+enum MovableObject {
+    case windows (WindowsAction)
+    case doors (DoorsAction)
+}
+
+enum WindowsAction {
+    case open, close
+}
+
+enum DoorsAction {
+    case open, close
+}
+
+
+class Car {
+    let brand: String
+    let color: UIColor
+    let transmission: Transmission
+    var km: Double
+    var isWindowsOpen: Bool = false
+    var isDoorsOpen: Bool = false
+    
+    init(brand: String, color: UIColor, transmission: Transmission, km: Double) {
+        self.brand = brand
+        self.color = color
+        self.transmission = transmission
+        self.km = km
+    }
+    
+    static func Cars() {
+        
+    }
+    
+}
